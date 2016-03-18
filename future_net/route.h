5dc8adbe37ac70ab379132e72f8e02fcf8ebfe61
chinazhonghao <1825547767@qq.com>
2016-03-18 21:57:25 +0800

    Test for change2List Function

--- future_net/route.h
+++ future_net/route.h
@@ -6,11 +6,10 @@ void search_route(char *graph[5000], int edge_num, char *condition);
 const int MAX_VERTEX_NUM = 600;		// max vertex number in the graph
 const int MAX_INCLUDING_SET = 50;	// max vertex number in the including set
 
-
 typedef struct EdgeNode
 {
-	int edgeID;
-	int nextID;
+	int linkID;
+	int nodeID;
 	int weight;
 	EdgeNode *next;
 }EdgeNode;
@@ -19,5 +18,8 @@ void getTopoArray(int edge_num, char *topo[5000], int topoArray[][4]);
 
 void getDemand(char *demand, int includingSet[MAX_INCLUDING_SET], int& sourceID, int& destinationID, int& cntPass);
 
+void change2List(EdgeNode *node[MAX_VERTEX_NUM],int topoArray[][4],int edge_num);
+
+void testChange2List(EdgeNode *node[MAX_VERTEX_NUM]);
 
 #endif
