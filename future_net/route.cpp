be3609edc0a9fbc12cb15237bda5dbbd51753332
Mya <448101470@qq.com>
2016-03-20 21:33:39 +0800

    Informal Commit.
    Dijkstra funciton is finished.
    This commit is done so as to transfer work to linux environment.

--- future_net/route.cpp
+++ future_net/route.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <queue>
 #include <memory.h>
+using namespace std;
 
 //你要完成的功能总入口
 void search_route(char *topo[5000], int edge_num, char *demand)
@@ -13,13 +14,25 @@ void search_route(char *topo[5000], int edge_num, char *demand)
 	// 		linkID, sourceID, destinationID, cost (end of line is not \n!)
 	int topoArray[edge_num][4];
 	int includingSet[MAX_INCLUDING_SET];
+	int cntNode;					// total amount of nodes in the graph.
 	int cntPass;					// record valid number of elements in includingSet.
 	int sourceID = 0, destinationID = 0;
 	memset(topoArray, 0, sizeof(topoArray));	// initialize to 0.
-	memset(includingSet, 0, sizeof(includingSet));
+	memset(includingSet, 0, sizeof(includingSet));	// 0 could be a node in includingSet. initialization of 0 is not safe???
+	//for(int i = 0; i < MAX_INCLUDING_SET; i++)
+	//	includingSet[i] = -1;
 
 	getTopoArray(edge_num, topo, topoArray);
+	//cntNode = cntNodeNum(topoArray);
+	printf("cntNode = %d\n",cntNode);
 	getDemand(demand, includingSet, sourceID, destinationID, cntPass);
+
+	int passSet[MAX_INCLUDING_SET+2];		// passSet = source + includingSet + destination.
+	passSet[0] = sourceID;
+	for(int i = 0; i < cntPass; i++)
+		passSet[i+1] = includingSet[i];
+	passSet[cntPass+1] = destinationID;
+
 
 	//================================================================================
 	// CREAT ADJACENT LIST.
@@ -30,10 +43,42 @@ void search_route(char *topo[5000], int edge_num, char *demand)
 	testChange2List(nodeArray);
 #endif // DEBUG
 
-	bool nodeArrayState[MAX_VERTEX_NUM];
-	memset(nodeArray,0,sizeof(nodeArrayState));
 
-	//priority_queue<int,vector<int>,greater<int> > pq;
+	int shortestDistSet[MAX_INCLUDING_SET+2][MAX_INCLUDING_SET+2];	// source and destination included.(actually used [cntPass+2][cntPass+2])
+	int pathSet[MAX_INCLUDING_SET][MAX_VERTEX_NUM];			// (actually used: [cntPass+2][MAX_VERTEX_NUM])
+	int ds[MAX_VERTEX_NUM];		// record distance between node s and other nodes.
+	int father[MAX_VERTEX_NUM];	// record father of each node. -1: no father.
+	memset(shortestDistSet, INF, sizeof(shortestDistSet));
+	memset(pathSet, -1, sizeof(pathSet));
+	
+	// calculate shortest path between each pair of node in the passSet.
+	for(int i = 0; i < cntPass + 2; i++)
+	{
+		dijkstra(passSet[i], ds, nodeArray, father);
+		for(int j = 0; j < cntPass + 2; j++)
+			shortestDistSet[i][j] = ds[j];
+		for(int j = 0; j < MAX_VERTEX_NUM; j++)
+			pathSet[i][j] = father[j];
+
+
+	}
+
+	//for()
+
+
+	/*
+	dijkstra(0, ds, nodeArray, father);
+	for(int i = 0; i < 10; i++)
+		printf("distance from node0 to node%d is %d.\n", i, ds[i]);
+	
+	int dad = father[3];
+	while(dad != -1)
+	{
+		printf("%d->",dad);
+		dad = father[dad];
+	}
+	*/
+
 	unsigned short result[] = {2, 6, 3};//示例中的一个解
 	for (int i = 0; i < 3; i++)
 		record_result(result[i]);
@@ -165,3 +210,91 @@ void testChange2List(EdgeNode *node[MAX_VERTEX_NUM])
 		}
 	}
 }
+
+
+/*
+int cntNodeNum(int topoArray[MAX_VERTEX_NUM*8])
+{
+	int cntArray[MAX_VERTEX_NUM];
+	memset(cntArray, 0, sizeof(cntArray));
+	int cnt = 0;
+	for(int i = 0; i < MAX_VERTEX_NUM*8; i++)
+	{
+		int n1 = topoArray[i][1];
+		int n2 = topoArray[i][2];
+		cntArray[n1] = 1;
+		cntArray[n2] = 1;
+	}
+	for(int i = 0; i < MAX_VERTEX_NUM*8; i++)
+		if(cntArray[i] != 0)
+			cnt++;
+	return cnt;
+}
+*/
+
+
+/*
+void dijkstra(int s, int ds[MAX_VERTEX_NUM], EdgeNode* node[MAX_VERTEX_NUM])		// distance from node s to each node.
+{
+	priority_queue<HeapNode> Q;
+	bool done[MAX_VERTEX_NUM];
+	for(int i = 0; i < n; i++) ds[i] = INF;
+	ds[s] = 0;
+	memset(done, 0, sizeof(done));
+	Q.push((HeapNode){0,s});
+	while(!Q.empty()){
+		HeapNode x = Q.top(); Q.pop();
+		int u = x.u;
+		if(done[u]) continue;
+		done[u] = true;
+		for(int i = 0; i < G[u].size(); i++){
+			Edge& e = edges[G[u][i]];
+			if(d[e.to] > ds[u] + e.dist){
+				ds[e.to] = ds[u] + e.dist;
+				p[e.to] = G[u][i];
+				Q.push((HeapNode{d[e.to], e.to}));
+			}
+		}
+	}
+}
+*/
+
+void dijkstra(int s, int ds[MAX_VERTEX_NUM], EdgeNode* nodeArray[MAX_VERTEX_NUM], int father[MAX_VERTEX_NUM])
+{
+	priority_queue<HeapNode> Q;
+	bool done[MAX_VERTEX_NUM];
+	
+	// INITIALIZE-SINGLE-SOURCE(G,s)
+	for(int i = 0; i < MAX_VERTEX_NUM; i++){
+		ds[i] = INF;
+		father[i] = -1;
+	}
+	ds[s] = 0;
+
+	memset(done, 0, sizeof(done));
+	Q.push((HeapNode){0,s});
+	while(!Q.empty()){
+		HeapNode x = Q.top(); Q.pop();
+		//printf("queue out ID:%d\n",x.endID);
+		int ID = x.endID;			// first round: ID = s.
+		if(done[ID]) continue;
+		done[ID] = true;
+		if(nodeArray[ID] != NULL){
+			EdgeNode* traverse = nodeArray[ID];	// traverse initially points to the first edge(if at least 1 edge exists).
+			while(traverse != NULL){
+				if(ds[traverse->nodeID] > ds[ID] + traverse->weight){
+					ds[traverse->nodeID] = ds[ID] + traverse->weight;
+					father[traverse->nodeID] = ID;
+					Q.push((HeapNode){ds[traverse->nodeID],traverse->nodeID});
+					//printf("queue in ID:%d\n",traverse->nodeID);
+				}
+				traverse = traverse -> next;
+			}
+		}
+	}
+
+}
+
+
+
+
--- future_net/route.h
+++ future_net/route.h
@@ -5,8 +5,9 @@ void search_route(char *graph[5000], int edge_num, char *condition);
 
 const int MAX_VERTEX_NUM = 600;		// max vertex number in the graph
 const int MAX_INCLUDING_SET = 50;	// max vertex number in the including set
+const int INF = 1000000;			// weight is integer between 1 and 20
 
-#define ROUTEDEBUG
+//#define ROUTEDEBUG
 
 typedef struct EdgeNode
 {
@@ -16,6 +17,18 @@ typedef struct EdgeNode
 	EdgeNode *next;
 } EdgeNode;
 
+struct HeapNode		// for priority queue. containing distance and corresponding nodeID.
+{
+	int distance, endID;
+	//bool operator < (const HeapNode& rhs) const {
+	//	return distance > rhs.distance;
+	//}
+	friend bool operator < (HeapNode n1, HeapNode n2)
+	{
+		return n1.distance > n2.distance;		// I don't understand this shit... this behaves normally.
+	}							// if using '<', larger distance has higher priority...
+};
+
 void getTopoArray(int edge_num, char *topo[5000], int topoArray[][4]);
 
 void getDemand(char *demand, int includingSet[MAX_INCLUDING_SET], int& sourceID, int& destinationID, int& cntPass);
@@ -23,5 +36,9 @@ void getDemand(char *demand, int includingSet[MAX_INCLUDING_SET], int& sourceID,
 void change2List(EdgeNode *node[MAX_VERTEX_NUM],int topoArray[][4],int edge_num);
 
 void testChange2List(EdgeNode *node[MAX_VERTEX_NUM]);
+
+int cntNodeNum(int topoArray[MAX_VERTEX_NUM*8]);
+
+void dijkstra(int s, int ds[MAX_VERTEX_NUM], EdgeNode* nodeArray[MAX_VERTEX_NUM], int father[MAX_VERTEX_NUM]);
 
 #endif
